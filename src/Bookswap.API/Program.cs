3cedf540297563812bb95dea85a4267c461199cf
simeonov90 <vilensimeonov@gmail.com>
2022-12-22 18:12:54 +0200

    Program.cs -> register service and add connection string

--- src/Bookswap.API/Program.cs
+++ src/Bookswap.API/Program.cs
@@ -1,11 +1,24 @@
+using Bookswap.Application.AutoMapperProfiles;
+using Bookswap.Domain.DbContext;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+
+var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
+builder.Services.AddDbContext<BookswapDbContext>(options =>
+{
+    options.UseSqlServer(connectionString);
+});
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+builder.Services.AddHttpContextAccessor();
+builder.Services.AddSingleton<IHttpContextAccessor, HttpContextAccessor>();
+builder.Services.AddAutoMapper(typeof(AutoMapperProfile));
 
 var app = builder.Build();
 
