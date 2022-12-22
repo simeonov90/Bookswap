470ca35c92ba1399e304fc163ed2fc2fff118f1d
simeonov90 <vilensimeonov@gmail.com>
2022-12-22 18:10:40 +0200

    added Audited entity

---
+++ src/Bookswap.Domain/Extensions/Entities/IEntities/IAuditedEntity.cs
@@ -0,0 +1,11 @@
+﻿namespace Bookswap.Domain.Extensions.Entities.IEntities
+{
+    public interface IAuditedEntity
+    {
+        public string? UserCreatorId { get; set; }
+        public DateTime CreationDateTime { get; set; }
+        public string? LastModifiedUserId { get; set; }
+        public DateTime? LastModifiedDateTime { get; set; }
+        public bool IsDeleted { get; set; }
+    }
+}
