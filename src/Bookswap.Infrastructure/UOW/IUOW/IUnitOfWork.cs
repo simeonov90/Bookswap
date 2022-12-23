8663db81334d8d10c44ad663e04c7b6e608a224d
simeonov90 <vilensimeonov@gmail.com>
2022-12-23 18:13:29 +0200

    added  UnitOfWork

---
+++ src/Bookswap.Infrastructure/UOW/IUOW/IUnitOfWork.cs
@@ -0,0 +1,10 @@
+﻿using Bookswap.Infrastructure.Repository.IRepository;
+
+namespace Bookswap.Infrastructure.UOW.IUOW
+{
+    public interface IUnitOfWork : IDisposable
+    {
+        IAuthorRepository Authors { get; }
+        Task<int> CompletedAsync();
+    }
+}
---
+++ src/Bookswap.Infrastructure/UOW/UnitOfWork.cs
@@ -0,0 +1,28 @@
+﻿using Bookswap.Domain.DbContext;
+using Bookswap.Infrastructure.Repository;
+using Bookswap.Infrastructure.Repository.IRepository;
+using Bookswap.Infrastructure.UOW.IUOW;
+using Microsoft.Extensions.Logging;
+
+namespace Bookswap.Infrastructure.UOW
+{
+    public class UnitOfWork : IUnitOfWork
+    {
+        private readonly BookswapDbContext dbContext;
+        private readonly ILogger logger;
+
+        public IAuthorRepository Authors { get; private set; }
+
+        public UnitOfWork(BookswapDbContext dbContext, ILogger logger)
+        {
+            this.dbContext = dbContext;
+            this.logger = logger;
+
+            Authors = new AuthorRepository(dbContext, logger);
+        }
+
+        public async Task<int> CompletedAsync() => await dbContext.SaveChangesAsync();
+
+        public void Dispose() => dbContext.Dispose();
+    }
+}
