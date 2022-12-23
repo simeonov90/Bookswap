348347f7ee82c181a8b2bcdc6104963bb410afff
simeonov90 <vilensimeonov@gmail.com>
2022-12-23 18:12:41 +0200

    added Author service

---
+++ src/Bookswap.Application/Services/Authors/AuthorService.cs
@@ -0,0 +1,25 @@
+﻿using Bookswap.Domain.Models;
+using Bookswap.Infrastructure.UOW.IUOW;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bookswap.Application.Services.Authors
+{
+    public class AuthorService
+    {
+        private readonly IUnitOfWork unitOfWork;
+
+        public AuthorService(IUnitOfWork unitOfWork)
+        {
+            this.unitOfWork = unitOfWork;
+        }
+
+        public async Task<IEnumerable<Author>> GetAll()
+        {
+            return await unitOfWork.Authors.GetAll();
+        }
+    }
+}
