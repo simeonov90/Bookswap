b599de6a59d6e20b3e004e19bb5f83a571da9a9e
simeonov90 <vilensimeonov@gmail.com>
2022-12-31 16:38:11 +0200

    UI -> added login and signup components

--- src/Bookswap.UI/src/app/app.module.ts
+++ src/Bookswap.UI/src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoginComponent } from './components/account/login/login.component';
+import { SignupComponent } from './components/account/signup/signup.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    SignupComponent
   ],
   imports: [
     BrowserModule,
