f118b7e2399bd3122102683b730e1ad75bfe7824
simeonov90 <vilensimeonov@gmail.com>
2022-12-31 14:58:17 +0200

    static method get Current user id from ClaimsPrinciple

---
+++ src/Bookswap.Infrastructure/Extensions/ClaimsPrincipalExtensions.cs
@@ -0,0 +1,12 @@
+﻿using System.Security.Claims;
+
+namespace Bookswap.Infrastructure.Extensions
+{
+    public static class ClaimsPrincipalExtensions
+    {
+        public static string CurrentUserId(this ClaimsPrincipal claimsPrincipal)
+        {
+            return claimsPrincipal.FindFirstValue(ClaimTypes.NameIdentifier);
+        }
+    }
+}
