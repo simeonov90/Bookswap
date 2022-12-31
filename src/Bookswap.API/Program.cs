aef6da6aee4e3c6e7243b1f07961a21bfed3890c
simeonov90 <vilensimeonov@gmail.com>
2022-12-31 14:55:51 +0200

    Program.cs -> Serilog and Identity

--- src/Bookswap.API/Program.cs
+++ src/Bookswap.API/Program.cs
@@ -1,10 +1,23 @@
 using Bookswap.Application.AutoMapperProfiles;
+using Bookswap.Application.Services.Authors;
+using Bookswap.Application.Services.Accounts;
 using Bookswap.Domain.DbContext;
+using Bookswap.Domain.Models;
+using Bookswap.Infrastructure.Repository;
+using Bookswap.Infrastructure.Repository.IRepository;
+using Bookswap.Infrastructure.UOW;
+using Bookswap.Infrastructure.UOW.IUOW;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
+using Serilog;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+builder.Services.AddScoped<IUnitOfWork, UnitOfWork>();
+builder.Services.AddScoped<IAuthorRepository, AuthorRepository>();
+builder.Services.AddTransient<IAuthorService, AuthorService>();
+builder.Services.AddTransient<IAccountService, AccountService>();
 
 var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
 builder.Services.AddDbContext<BookswapDbContext>(options =>
@@ -12,13 +25,47 @@ builder.Services.AddDbContext<BookswapDbContext>(options =>
     options.UseSqlServer(connectionString);
 });
 
+var logger = new LoggerConfiguration()
+    .ReadFrom.Configuration(builder.Configuration)
+    .Enrich.FromLogContext()
+    .CreateLogger();
+
+builder.Logging.ClearProviders();
+builder.Logging.AddSerilog(logger);
+builder.Logging.AddConsole();
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 builder.Services.AddHttpContextAccessor();
+builder.Services.AddLogging();
 builder.Services.AddSingleton<IHttpContextAccessor, HttpContextAccessor>();
 builder.Services.AddAutoMapper(typeof(AutoMapperProfile));
+
+builder.Services.AddIdentityCore<BookswapUser>(options =>
+{
+    options.SignIn.RequireConfirmedAccount = false;
+    options.Password.RequireDigit = false;
+    options.Password.RequireLowercase = false;
+    options.Password.RequireUppercase = false;
+    options.Password.RequiredLength = 5;
+    options.Password.RequiredUniqueChars = 0;
+    options.Password.RequireNonAlphanumeric = false;
+})
+    .AddRoles<IdentityRole>()
+    .AddEntityFrameworkStores<BookswapDbContext>();
+
+builder.Services.AddCors(options =>
+{
+
+    options.AddDefaultPolicy(
+        policy =>
+        {
+            policy.AllowAnyOrigin()
+                .AllowAnyHeader()
+                .AllowAnyMethod();
+        });
+});
 
 var app = builder.Build();
 
@@ -34,5 +81,7 @@ app.UseHttpsRedirection();
 app.UseAuthorization();
 
 app.MapControllers();
+
+app.UseCors();
 
 app.Run();
