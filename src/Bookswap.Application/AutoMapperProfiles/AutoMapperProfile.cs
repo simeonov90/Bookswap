ab9ec50b9b903eeeb314df4c2954a28a632608e0
simeonov90 <vilensimeonov@gmail.com>
2023-01-09 16:09:37 +0200

    added AutoMapper profile

--- src/Bookswap.Application/AutoMapperProfiles/AutoMapperProfile.cs
+++ src/Bookswap.Application/AutoMapperProfiles/AutoMapperProfile.cs
@@ -1,12 +1,19 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using AutoMapper;
+using Bookswap.Application.Services.Authors.Dto;
+using Bookswap.Application.Services.Accounts.Dto;
+using Bookswap.Domain.Models;
 
 namespace Bookswap.Application.AutoMapperProfiles
 {
-    public class AutoMapperProfile
+    public class AutoMapperProfile : Profile
     {
+        public AutoMapperProfile()
+        {
+            CreateMap<CreateAuthorDto, Author>();
+            CreateMap<UpdateAuthorDto, Author>();
+            CreateMap<Author, AuthorDto>();
+
+            CreateMap<UserDto,  BookswapUser>();
+        }
     }
 }
