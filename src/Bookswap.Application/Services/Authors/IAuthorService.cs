9a1faabf045fa82fdb2c7777ae30730c0e4c8989
simeonov90 <vilensimeonov@gmail.com>
2023-01-19 20:23:07 +0200

    added get all paging method to interface

--- src/Bookswap.Application/Services/Authors/IAuthorService.cs
+++ src/Bookswap.Application/Services/Authors/IAuthorService.cs
@@ -1,10 +1,12 @@
 ﻿using Bookswap.Application.Services.Authors.Dto;
+using Bookswap.Infrastructure.Extensions.Models;
 
 namespace Bookswap.Application.Services.Authors
 {
     public interface IAuthorService
     {
         Task<IEnumerable<AuthorDto>> GetAllAsync();
+        Task<PagingPagedResult<AuthorDto>> GetAllAsync(PagingQueryParameters queryParameters);
         Task<AuthorDto> GetById(int id);
         Task<AuthorDto> CreateAsync(CreateAuthorDto createAuthorDto);
         Task UpdateAsync(UpdateAuthorDto updateAuthorDto);
