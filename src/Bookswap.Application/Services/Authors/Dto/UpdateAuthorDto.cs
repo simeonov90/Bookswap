1fed789efd93706df2d3715ec745b83eece8d5f7
simeonov90 <vilensimeonov@gmail.com>
2023-01-19 20:21:41 +0200

    update author dto validation

--- src/Bookswap.Application/Services/Authors/Dto/UpdateAuthorDto.cs
+++ src/Bookswap.Application/Services/Authors/Dto/UpdateAuthorDto.cs
@@ -1,8 +1,13 @@
-﻿namespace Bookswap.Application.Services.Authors.Dto
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Bookswap.Application.Services.Authors.Dto
 {
     public class UpdateAuthorDto
     {
+        [Required]
         public int Id { get; set; }
+        [Required]
+        [StringLength(30)]
         public string FullName { get; set; }
     }
 }
