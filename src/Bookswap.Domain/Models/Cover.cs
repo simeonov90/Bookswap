22d77c9f0b7f6403eb29dd8c6060aec83bf2e911
simeonov90 <vilensimeonov@gmail.com>
2023-01-21 23:13:39 +0200

    add new entity -> Cover

---
+++ src/Bookswap.Domain/Models/Cover.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Bookswap.Domain.Models
+{
+    public class Cover
+    {
+        [Key]
+        public Guid Id { get; set; }
+        public byte[] Bytes { get; set; }
+        public string FileExtension { get; set; }
+        public decimal Size { get; set; }
+        //public int BookId { get; set; }
+        //[ForeignKey(nameof(BookId))]
+        //public Book Book { get; set; }
+    }
+}
