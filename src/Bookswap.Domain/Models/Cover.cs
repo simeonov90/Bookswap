d978f68e8d1e4e0967b4851d00b2fe389ef4eb71
simeonov90 <vilensimeonov@gmail.com>
2023-01-22 11:29:41 +0200

    added property to entity model Cover

--- src/Bookswap.Domain/Models/Cover.cs
+++ src/Bookswap.Domain/Models/Cover.cs
@@ -7,6 +7,7 @@ namespace Bookswap.Domain.Models
         [Key]
         public Guid Id { get; set; }
         public byte[] Bytes { get; set; }
+        public string Description { get; set; }
         public string FileExtension { get; set; }
         public decimal Size { get; set; }
         //public int BookId { get; set; }
