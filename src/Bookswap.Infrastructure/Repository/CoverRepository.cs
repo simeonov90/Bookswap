a2fc9d21bd81182ddc925bf53a4fc6c28455678b
simeonov90 <vilensimeonov@gmail.com>
2023-01-25 17:16:12 +0200

    add Cover repository

---
+++ src/Bookswap.Infrastructure/Repository/CoverRepository.cs
@@ -0,0 +1,16 @@
+﻿using AutoMapper;
+using Bookswap.Domain.DbContext;
+using Bookswap.Domain.Models;
+using Bookswap.Infrastructure.Repository.IRepository;
+
+namespace Bookswap.Infrastructure.Repository
+{
+    public class CoverRepository : GenericRepository<Cover, Guid>, ICoverRepository
+    {
+        public CoverRepository(BookswapDbContext dbContext, IMapper mapper)
+            :base(dbContext, mapper)
+        {
+            
+        }
+    }
+}
