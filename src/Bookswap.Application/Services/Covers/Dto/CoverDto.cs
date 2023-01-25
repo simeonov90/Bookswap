b025be1df5307673c026507681d7d18dca0aa3f5
simeonov90 <vilensimeonov@gmail.com>
2023-01-25 17:15:37 +0200

    add Cover dtos

---
+++ src/Bookswap.Application/Services/Covers/Dto/CoverDto.cs
@@ -0,0 +1,13 @@
+﻿namespace Bookswap.Application.Services.Covers.Dto
+{
+    public class CoverDto
+    {
+        public Guid? Id { get; set; }
+        public byte[]? Bytes { get; set; }
+        public string? Description { get; set; }
+        public string? FileExtension { get; set; }
+        public decimal? Size { get; set; }
+        public string? ReadableFileSize { get; set; }
+        public string? UnSupportedFileType { get; set;}
+    }
+}
