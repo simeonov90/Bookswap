c78082c5cc2f9228def7e8f76dfff07b2ad0d942
simeonov90 <vilensimeonov@gmail.com>
2023-01-25 17:15:19 +0200

    add extension classes for human readable file size and supported file types

---
+++ src/Bookswap.Application/Extensions/Methods/HumanReadableFileSize.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bookswap.Application.Extensions.Methods
+{
+    public static class HumanReadableFileSize
+    {
+        public static string ReadableFileSize(long fileLength)
+        {
+            string[] sizes = { "B", "KB", "MB", "GB", "TB" };
+            double len = fileLength;
+            int order = 0;
+            while (len >= 1024 && order < sizes.Length - 1)
+            {
+                order++;
+                len = len / 1024;
+            }
+
+            return string.Format("{0:0.##} {1}", len, sizes[order]);
+        }
+    }
+}
---
+++ src/Bookswap.Application/Extensions/Methods/SupportedFileTypes.cs
@@ -0,0 +1,14 @@
+﻿namespace Bookswap.Application.Extensions.Methods
+{
+    public static class SupportedFileTypes
+    {
+        private static string[] supportedImageTypes = new string[] { "jpg", "jpeg", "png", "pdf", "gif" };
+
+        public static bool IsValidImageType(this string fileExtension)
+        {
+            if (!supportedImageTypes.Contains(fileExtension.Substring(1))) return false;
+
+            return true;
+        }
+    }
+}
