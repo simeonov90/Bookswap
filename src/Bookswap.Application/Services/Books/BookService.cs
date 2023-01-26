50d63acc756f76569602f4d85956e5d8b88597db
simeonov90 <vilensimeonov@gmail.com>
2023-01-26 09:14:37 +0200

    add BookService class

--- src/Bookswap.Application/Services/Books/BookService.cs
+++ src/Bookswap.Application/Services/Books/BookService.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using Bookswap.Application.Services.Books.Dtos;
+using Bookswap.Infrastructure.Extensions.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +8,41 @@ using System.Threading.Tasks;
 
 namespace Bookswap.Application.Services.Books
 {
-    internal class BookService
+    public class BookService : IBookService
     {
+        public Task<BookDto> CreateAsync(CreateBookDto createAuthorDto)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task DeleteAsync(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<bool> Exists(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<IEnumerable<BookDto>> GetAllAsync()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<PagingPagedResult<BookDto>> GetAllAsync(PagingQueryParameters queryParameters)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<BookDto> GetById(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task UpdateAsync(UpdateBookDto updateAuthorDto)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
